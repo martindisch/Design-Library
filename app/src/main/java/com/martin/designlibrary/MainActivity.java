5cace305e7f28ea5808258ea93571f783a80187c
Martin Disch <martindisch@gmail.com>
2015-05-29 23:32:42 +0200

    Polished solution without 'original' ActionBar

--- app/src/main/java/com/martin/designlibrary/MainActivity.java
+++ app/src/main/java/com/martin/designlibrary/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         if (viewPager != null) {
             setupViewPager(viewPager);
@@ -40,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Great job", Snackbar.LENGTH_LONG).setAction("Got it", new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {}
+                    public void onClick(View view) {
+                    }
                 }).show();
             }
         });
