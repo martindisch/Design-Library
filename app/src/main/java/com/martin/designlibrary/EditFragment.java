f00fc18847216ab7e09f2b87f103cf762d0a30a9
Martin Disch <martindisch@gmail.com>
2015-05-30 21:08:04 +0200

    Added the required empty Constructor

--- app/src/main/java/com/martin/designlibrary/EditFragment.java
+++ app/src/main/java/com/martin/designlibrary/EditFragment.java
@@ -19,7 +19,8 @@ public class EditFragment extends Fragment {
         return fragment;
     }
 
-    @Nullable
+    public EditFragment() {}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_edit, container, false);
