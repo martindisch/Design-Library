ea6122f71353d2fae97f9138efa9cc641d8b83ef
Martin Disch <martindisch@gmail.com>
2015-05-30 21:22:18 +0200

    Setting title in CollapsingToolbarLayout

--- app/src/main/java/com/martin/designlibrary/CollapsingFragment.java
+++ app/src/main/java/com/martin/designlibrary/CollapsingFragment.java
@@ -1,9 +1,13 @@
 package com.martin.designlibrary;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +32,17 @@ public class CollapsingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_collapsing, container, false);
+
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout ctl = (CollapsingToolbarLayout) rootView.findViewById(R.id.collapsing_toolbar);
+        ctl.setTitle("Collapse");
+
         mList = (RecyclerView) rootView.findViewById(R.id.rvContents);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
