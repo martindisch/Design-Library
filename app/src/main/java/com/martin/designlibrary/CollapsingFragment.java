fd060fa04f381052275e0c5d411eb6fbaf85f4e7
Martin Disch <martindisch@gmail.com>
2015-05-30 20:19:17 +0200

    Collapsing Toolbar with RecyclerView

--- app/src/main/java/com/martin/designlibrary/CollapsingFragment.java
+++ app/src/main/java/com/martin/designlibrary/CollapsingFragment.java
@@ -2,11 +2,15 @@ package com.martin.designlibrary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class CollapsingFragment extends Fragment {
+
+    private RecyclerView mList;
 
     public static CollapsingFragment newInstance() {
         CollapsingFragment fragment = new CollapsingFragment();
@@ -24,6 +28,12 @@ public class CollapsingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_collapsing, container, false);
+        mList = (RecyclerView) rootView.findViewById(R.id.rvContents);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mList.setLayoutManager(layoutManager);
+        mList.setHasFixedSize(true);
+        mList.setAdapter(new ListAdapter());
         return rootView;
     }
 
